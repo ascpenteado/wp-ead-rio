ab7b8221a1c4d2346ab7038c88f8a21dfa35ca79
Arthur Penteado <arthurp@thoughtlab.com>
2025-09-27 22:36:47 -0300

    feat: Simplify site branding section in header.php

--- web/wp-content/themes/ead-rio/header.php
+++ web/wp-content/themes/ead-rio/header.php
@@ -25,24 +25,7 @@
 
     <header id="masthead" class="site-header">
         <div class="container">
-            <div class="site-branding">
-                <?php
-                the_custom_logo();
-                if (is_front_page() && is_home()) :
-                    ?>
-                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-                    <?php
-                else :
-                    ?>
-                    <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-                    <?php
-                endif;
-                $ead_rio_description = get_bloginfo('description', 'display');
-                if ($ead_rio_description || is_customize_preview()) :
-                    ?>
-                    <p class="site-description"><?php echo $ead_rio_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-                <?php endif; ?>
-            </div>
+            <div class="site-branding"><?php the_custom_logo(); ?></div>
 
             <nav id="site-navigation" class="main-navigation">
                 <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">

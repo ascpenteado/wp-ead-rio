dbd957ad2d4b1bff0b806894f98f3db3499bf9eb
Arthur Penteado <ascpenteado@gmail.com>
2025-11-30 19:07:45 -0300

    feat: Introduce DDEV for local development, add Railway deployment guide, and update the Rio Cards module.

--- web/wp-content/themes/ead-rio/bs-config.js
+++ web/wp-content/themes/ead-rio/bs-config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  proxy: "http://localhost", // Docker Compose WordPress site URL
-  files: [
-    "dist/css/**/*.css",
-    "dist/js/**/*.js",
-    "**/*.php"
-  ],
-  watchEvents: ["change"],
-  ignore: [
-    "node_modules",
-    "*.map"
-  ],
+  proxy: 'https://wp-ead-rio.ddev.site', // DDEV WordPress site URL
+  files: ['dist/css/**/*.css', 'dist/js/**/*.js', '**/*.php'],
+  watchEvents: ['change'],
+  ignore: ['node_modules', '*.map'],
   injectChanges: true,
   open: false,
   notify: true,
-  reloadDelay: 100
+  reloadDelay: 100,
 };
